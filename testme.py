a3bb6f1bd12b4a214e69e18328c8dcbdb4c2ecbc
kinzydaniel54 <kinzydaniel54@icloud.com>
2024-07-21 14:13:17 -0400

    testme.py

---
+++ testme.py
@@ -0,0 +1,16 @@
+#my full name
+my_full_name = "Daniel Kinzy"
+print(my_full_name)
+#two variabales
+myName = "daniel"
+myname = "kinzy"
+print(myName, myname)
+#list
+x = "apple"
+y = "banana"
+z = "cherry"
+print(x, y, z)
+#mysteryvar
+mystery_var = 1234
+print(type(mystery_var))
+
