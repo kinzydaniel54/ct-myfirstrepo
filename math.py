a2c8e0170363b90e1a2a7968d7b2da7df940b6e7
kinzydaniel54 <kinzydaniel54@icloud.com>
2024-07-21 13:01:27 -0400

    math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+a = 5
+#
+print(type(a))
+#
+a = 5
+a = "daniel"
+print(a)
+
+x = "cherry, tree, apples"
+print(x)
+
+b = "volvo_is_a_bad_car."
+z = "toyota_is_a_great_car"
+
+print(b)
+print(z)
+
+#a is 5 whith a type class in
+#line 5-6 is 5 with a different variable named daniel
+#line 12-16 are variables following pythons naming conventions
